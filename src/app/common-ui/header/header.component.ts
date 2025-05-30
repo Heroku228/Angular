ab40be7ea169814526061e61bb91c3a94fed0320
Ivan <iv4novichi@yandex.com>
2025-05-30 21:23:03 +0200

    Добавлен компонент Header

--- src/app/common-ui/header/header.component.ts
+++ src/app/common-ui/header/header.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { Router, RouterModule } from '@angular/router'
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html'
+	selector: 'app-header',
+	standalone: true,
+	imports: [RouterModule, CommonModule],
+	templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+	constructor(private readonly router: Router) { }
 
+	routes = [
+		{ label: 'Home', path: '/' },
+		{ label: 'News', path: '/news' },
+		{ label: 'About', path: '/about' },
+		{ label: 'Cart', path: '/cart' },
+		{ label: 'Account', path: '/i' }
+	];
 }
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,11 @@
+// import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router'
+// import { ApiUsersService } from '../api/api-users.service'
+// import { inject } from '@angular/core'
+
+// export const authGuard: CanActivateFn = (route, state) => {
+// 	apiUsersService: ApiUsersService
+// 	router: ActivatedRouteSnapshot
+
+
+// 	return inject(ApiUsersService).c
+// }
